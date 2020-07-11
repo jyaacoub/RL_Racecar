06ab1618eb4297098dd369b7ec079e3b8018eedd
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-10 23:06:57 -0400

    init

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,8 +1,35 @@
 const screenW = 1500;
 const screenH =  840;
 
-function setup() {
-}
+let date = new Date();
 
+FR = 10;
+
+function setup() {
+    createCanvas(screenW, screenH);
+    background('Green');
+    frameRate(FR);
+}
+async function delay(delayInms) {
+    return new Promise(resolve  => {
+        setTimeout(() => {
+        resolve(2);
+        }, delayInms);
+    });
+}
+// async function sample() {
+//     console.log('a');
+//     console.log('waiting...')
+//     let delayres = await delay(3000);
+//     console.log('b');
+// }
 function draw() {
+    circle(30,30,30);
+    // background('Green');
+    var startTime = date.getTime();
+    while (date.getTime() - startTime < 1000){
+        console.log(startTime);
+    }
+    clear();
+    background('Green');
 }
