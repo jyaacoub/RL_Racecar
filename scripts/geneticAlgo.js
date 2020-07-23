613f58ed2716b39eb4aad4798640ebd3cc50c6c7
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-23 14:17:04 -0400

    setting up for GA

--- scripts/geneticAlgo.js
+++ scripts/geneticAlgo.js
@@ -1,2 +1,40 @@
 // TODO: Create a pipeline to connect the "frontend" graphics to the backend AI 
 // that controls the car
+
+class RL_controller {
+    constructor(car){
+        this.car = car;     // The car that it will be in control of.
+        
+    }
+    reciveInput(distances){
+        // The input thet the model recives are the values from 
+        // the sensors of the vehicle.
+    }
+    decideOnAction(){
+        // From the current distances it decides what action to perform next
+    }
+    performAction(car){
+        // There are 4 differant things the model can do:
+        //      Turn left, turn right, accelerate, and reverse.
+    }
+    get fitness(){
+
+    }
+}
+
+class GA_controller {
+    constructor(car){
+        this.car = car;     // The car that it will be in control of.
+        
+    }
+    decideOnAction(){
+        // From the current distances it decides what action to perform next
+    }
+    performAction(car){
+        // There are 4 differant things the model can do:
+        //      Turn left, turn right, accelerate, and reverse.
+    }
+    get fitness(goal){
+        
+    }
+}
